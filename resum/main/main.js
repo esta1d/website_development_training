1e6d8c5ab0841cc84048c84b95a0995cb4bdeaef
esta1d <skillet_skillet98@mail.ru>
2025-06-08 23:38:32 +0300

    site_learning

--- resum/main/main.js
+++ resum/main/main.js
@@ -1,12 +1,38 @@
-let coll = document.getElementsByClassName('collapse');
-for(let i = 0; i < coll.length; i++) {
-  coll[i].addEventListener('click', function() {
+// let coll = document.getElementsByClassName('collapse');
+// for(let i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener('click', function() {
+//     this.classList.toggle('active');
+//     let content = this.nextElementSibling;
+//     if (content.style.maxHeight) 
+//     {content.style.maxHeight = null;
+//     } else {
+//         content.style.maxHeight = content.scrollHeight + 'px'
+//     }
+//   })
+// }
+
+
+// Получаем все блоки ".myeducation-content"
+const blocks = document.querySelectorAll('.myeducation-content');
+
+blocks.forEach(block => {
+  block.addEventListener('click', function() {
+    // Находим соседний контент
+    const content = this.parentNode.querySelector('.content');
+    
+    // Переключение классов
     this.classList.toggle('active');
-    let content = this.nextElementSibling;
-    if (content.style.maxHeight) 
-    {content.style.maxHeight = null;
+    content.classList.toggle('active');
+
+    // Изменяем максимальную высоту контента
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
     } else {
-        content.style.maxHeight = content.scrollHeight + 'px'
+      content.style.maxHeight = content.scrollHeight + 'px';
     }
-  })
-}
+
+    // Меняем направление стрелочки
+    const arrow = this.querySelector('.arrow');
+    arrow.classList.toggle('arrowactive');
+  });
+});
